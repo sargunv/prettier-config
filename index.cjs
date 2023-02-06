d8eec414ee1f63588d7f2e0a833a048bbdf8deb1
Sargun Vohra <sargun.vohra@gmail.com>
2023-02-05 22:40:07 -0800

    eslint

--- index.cjs
+++ index.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-packagejson")],
-  htmlWhitespaceSensitivity: "ignore",
-  proseWrap: "always",
-  quoteProps: "consistent",
+  plugins: [require.resolve(`prettier-plugin-packagejson`)],
+  htmlWhitespaceSensitivity: `ignore`,
+  proseWrap: `always`,
+  quoteProps: `consistent`,
   semi: false,
   tabWidth: 2,
-  trailingComma: "all",
+  trailingComma: `all`,
   vueIndentScriptAndStyle: true,
 }
